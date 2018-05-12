ef2faa441c46d4a1e976356d27e5ae29eeab0603
SHIMADA Keiki <shimada.cake@gmail.com>
2018-05-12 20:41:25 +0900

    implement code and test

--- python/magnitude/temperature.py
+++ python/magnitude/temperature.py
@@ -1,22 +1,34 @@
+from typing import Union
+Numeric = Union[int,float]
+
 class Temperature:
+  def __init__(self, celsius: Numeric) -> None:
+    self._celsius: Numeric = celsius
 
   @classmethod
-  def celsius(cls, num):
-    return Temperature(num)
+  def from_celsius(cls, num: Numeric) -> 'Temperature':
+    return cls(num)
 
   @classmethod
-  def fahrenheit(cls, num):
-    return Temperature(f2c(num))
+  def from_fahrenheit(cls, num: Numeric) -> 'Temperature':
+    return cls(cls.f2c(num))
 
-  @classmethod
-  def f2c(cls, f):
+  @staticmethod
+  def f2c(f: Numeric) -> float:
     return (f - 32) / 1.8
 
-  def __init__(self, celsius):
-    self._celsius = celsius
+  @staticmethod
+  def c2f(c: Numeric) -> float:
+    return c * 1.8 + 32
 
-  def celsius(self):
+  def celsius(self) -> Numeric:
     return self._celsius
 
-  def fahrenheit(self):
-    pass
+  def fahrenheit(self) -> float:
+    return self.c2f(self._celsius)
+
+  def celsius_label(self) -> str:
+    return "摂氏%d度" % self.celsius()
+
+  def fahrenheit_label(self) -> str:
+    return "華氏%.2f度" % self.fahrenheit()
--- python/tests/temperature_test.py
+++ python/tests/temperature_test.py
@@ -3,10 +3,39 @@ from magnitude.temperature import Temperature
 
 class TemperatureTest(ut.TestCase):
   def test_celsius(self):
-    temp = Temperature.celsius(34)
-    print(temp)
+    temp = Temperature.from_celsius(34)
     self.assertEqual(34, temp.celsius())
 
   def test_fahrenheit(self):
-    pass
+    temp = Temperature.from_fahrenheit(12)
+    self.assertEqual(12, temp.fahrenheit())
+
+  def test_zero_c(self):
+    temp = Temperature.from_celsius(0)
+    self.assertEqual(32, temp.fahrenheit())
+
+  def test_zero_f(self):
+    temp = Temperature.from_fahrenheit(32)
+    self.assertEqual(0, temp.celsius())
+
+  def test_handred_c(self):
+    temp = Temperature.from_celsius(100)
+    self.assertEqual(212, temp.fahrenheit())
+
+  def test_handred_f(self):
+    temp = Temperature.from_fahrenheit(212)
+    self.assertEqual(100, temp.celsius())
+
+  def test_celsius_label(self):
+    temp = Temperature.from_celsius(34)
+    self.assertEqual("摂氏34度", temp.celsius_label())
+
+  def test_fahrenheit_label(self):
+    temp = Temperature.from_fahrenheit(12)
+    self.assertEqual("華氏12.00度", temp.fahrenheit_label())
+
+  def test_fail(self):
+    with self.assertRaises(Exception):
+      err = Temperature.from_celsius("abc")
+      err.fahrenheit()
 
